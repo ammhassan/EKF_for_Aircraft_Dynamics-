596eb57bde5b1443fca97e5b85d58a781a59b19c
Ahmed <am.hassan89@gmail.com>
2020-12-11 09:22:30 -0800

    Modified the structure of the LongDynamics class.

--- src/LongDynamics.h
+++ src/LongDynamics.h
@@ -8,9 +8,26 @@
 
 #define PI 3.14159265
 
-typedef std::vector<double> state_type;
+// Define the aircraft parameters
+static const struct AircraftParam {
+    const uint mass = 300000;
+    const float g = 9.81;
+    const double Xu = -0.02;
+    const double Xw = 0.1;
+    const double Zu = -0.23;
+    const double Zw = -0.634;
+    const double Mu = -0.0000255;
+    const double Mw = -0.005;
+    const double Mq = -0.61;
+    const double Xde = 0.14;
+    const double Zde = -2.9;
+    const double Mde = -0.64;
+    const double Xdth = 1.56;
+    const double Mdth = 0.0054;
+} param;
 
 // The rhs of x' = f(x, u) defined as a class
+typedef std::vector<double> state_type;
 class LongDynamics {
 public:
     LongDynamics() {}
@@ -25,36 +42,21 @@ public:
         double dth = 0;
         double de = 5 * PI / 180 * sin(PI * t);
 
-        dxdt[0] = -x[2] * x[1] - g * cos(theta0) * (x[3] - theta0) + Xu * (x[0] - U0)
-                + Xw * (x[1] - W0) + Xde * de + Xdth * dth;
-        dxdt[1] = x[2] * x[0] - g * sin(theta0) * (x[3] - theta0) + Zu * (x[0] - U0)
-                + Zw * (x[1] - W0) + Zde * de;
-        dxdt[2] = Mu * (x[0] - U0) + Mw * (x[1] - W0) + Mq * x[2] + Mde * de + Mdth * dth;
+        dxdt[0] = -x[2] * x[1] - param.g * cos(theta0_) * (x[3] - theta0_) + param.Xu * (x[0] - U0_)
+                + param.Xw * (x[1] - W0_) + param.Xde * de + param.Xdth * dth;
+        dxdt[1] = x[2] * x[0] - param.g * sin(theta0_) * (x[3] - theta0_) + param.Zu * (x[0] - U0_)
+                + param.Zw * (x[1] - W0_) + param.Zde * de;
+        dxdt[2] = param.Mu * (x[0] - U0_) + param.Mw * (x[1] - W0_) + param.Mq * x[2] 
+                + param.Mde * de + param.Mdth * dth;
         dxdt[3] = x[2];
     }
 
 private:
-    // Aircraft parameters
-    static constexpr uint mass = 300000;
-    static constexpr float g = 9.81;
-    static constexpr double Xu = -0.02;
-    static constexpr double Xw = 0.1;
-    static constexpr double Zu = -0.23;
-    static constexpr double Zw = -0.634;
-    static constexpr double Mu = -0.0000255;
-    static constexpr double Mw = -0.005;
-    static constexpr double Mq = -0.61;
-    static constexpr double Xde = 0.14;
-    static constexpr double Zde = -2.9;
-    static constexpr double Mde = -0.64;
-    static constexpr double Xdth = 1.56;
-    static constexpr double Mdth = 0.0054;
-
     // Initial condition
-    static constexpr double theta0 = 2.7 * PI / 180;
-    static constexpr double alpha0 = theta0;
-    static constexpr double U0 = 74.9167;
-    static constexpr double W0 = 3.533;
+    static constexpr double theta0_ = 2.7 * PI / 180;
+    static constexpr double alpha0_ = theta0_;
+    static constexpr double U0_ = 74.9167;
+    static constexpr double W0_ = 3.533;
 };
 
 #endif
